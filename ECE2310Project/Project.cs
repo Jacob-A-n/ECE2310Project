8810b2b560b5e9b620c4dd0dc032ea0ce09ff35d
Jacob-A-n <annjacobb1@gmail.com>
2026-04-16 19:24:25 -0700

    Add project files.

---
+++ ECE2310Project/Project.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ECE2310Project
+{
+    public class Project
+    {
+        static void Main()
+        {
+            //Initalizes GUI
+            Form windowForm = new Form();
+            windowForm.ShowDialog();
+        }
+    }
+}
+
